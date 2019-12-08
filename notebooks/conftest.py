e5b79cdbacb80e4090a05f4e7a717cbeb91871a2
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2019-12-08 13:14:57 -0800

    Setup continuous integration on GitHub Actions

---
+++ notebooks/conftest.py
@@ -0,0 +1,6 @@
+"""Test Configuration."""
+
+
+def pytest_collectstart(collector):
+    """Skip certain output types checked by nval plugin."""
+    collector.skip_compare += 'application/javascript', # 'text/html', 'stderr',
